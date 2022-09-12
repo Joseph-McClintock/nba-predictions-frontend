1bdf4e09d1a0f5e3a8f6c95d507eb5a8a9132810
Joseph McClintock <jmccli9@student.wgu.edu>
2022-09-11 23:43:47 -0700

    added more detail to info

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -4,7 +4,8 @@ function About() {
         <div className='about-container'>
             <h1>About page</h1>
             <p>This project was initially made for my college final and was made just using python and flask with html and css.
-                I've completely remade the frontend in react and I am using chartjs to display the data. The backend is
+                I've completely remade the frontend in react and I am using chartjs to display the data. The backend is made with
+                flask and deployed on render.
             </p>
         </div>
     )
